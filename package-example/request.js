8fa9fa2176f79708056e5e41156bc3e46c55d7d7
Luiz Felipe <luizfelipedasilva678@gmail.com>
2023-08-19 19:06:40 -0300

    Example with nodemon

--- package-example/request.js
+++ package-example/request.js
@@ -1,6 +1,6 @@
 const axios = require("axios").default;
 
 axios
-  .get("https://www.google.com")
+  .get("https://www.wikipedia.com")
   .then((resp) => console.log(resp))
   .catch((err) => console.log(err));
