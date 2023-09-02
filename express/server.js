a21730fb5977a4a3e54c0d360ff0136f46eec856
Luiz Felipe <luizfelipedasilva678@gmail.com>
2023-09-02 17:17:46 -0300

    Express middlewares

--- express/server.js
+++ express/server.js
@@ -2,8 +2,54 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello");
+const friends = [
+  {
+    id: 1,
+    name: "Sir Isaac Newton",
+  },
+];
+
+app.use((req, res, next) => {
+  const start = Date.now();
+  next();
+  const delta = Date.now() - start;
+  console.log(`${req.method} ${req.url} ${delta}ms`);
+});
+
+app.use(express.json());
+
+app.post("/friends", (req, res) => {
+  if (!req.body.name) {
+    return res.status(400).json({
+      error: "Invalid body",
+      errorMessage: "You must provide a name",
+    });
+  }
+
+  const newFriend = {
+    name: req.body.name,
+    id: friends.length,
+  };
+
+  friends.push(newFriend);
+  res.json(newFriend);
+});
+
+app.get("/friends", (req, res) => {
+  res.json(friends);
+});
+
+app.get("/friends/:friendId", (req, res) => {
+  const friendId = Number(req.params.friendId);
+  const friend = friends[friendId];
+
+  if (friend) {
+    res.status(200).json(friend);
+  } else {
+    res.status(404).json({
+      error: "Friend does not exist",
+    });
+  }
 });
 
 app.get("/messages", (req, res) => {
