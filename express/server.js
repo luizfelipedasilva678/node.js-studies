4bf137e322572c53dd8602e2301478d1b41f22a4
Luiz Felipe <luizfelipedasilva678@gmail.com>
2023-09-02 20:43:31 -0300

    Static files

--- express/server.js
+++ express/server.js
@@ -11,6 +11,7 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.url} ${delta}ms`);
 });
 
+app.use("/site", express.static("public"));
 app.use(express.json());
 app.use("/friends", friendsRouter);
 app.use("/messages", messagesRouter);
