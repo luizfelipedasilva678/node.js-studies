f42f27fb07889e003666133cc93e1aed77dd9eb8
Luiz Felipe <luizfelipedasilva678@gmail.com>
2023-10-03 19:41:10 -0300

    Async logic to get planets

--- nasa-project/server/src/models/planets.model.js
+++ nasa-project/server/src/models/planets.model.js
@@ -1,3 +1,42 @@
-const planets = [];
+const { parse } = require("csv-parse");
+const fs = require("fs");
+const path = require("path");
 
-module.exports = planets;
+const habitablePlanets = [];
+
+const filePath = path.resolve(__dirname, "..", "..", "data", "kepler_data.csv");
+
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
+const loadPlanetsData = () => {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(filePath)
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", (chunk) => {
+        if (isHabitablePlanet(chunk)) habitablePlanets.push(chunk);
+      })
+      .on("error", (err) => {
+        reject(err);
+      })
+      .on("end", () => {
+        resolve(habitablePlanets);
+      });
+  });
+};
+
+module.exports = {
+  loadPlanetsData,
+  planets: habitablePlanets,
+};
