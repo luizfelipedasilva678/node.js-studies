313e20d4e6111e89f6dcbeac5f6c806c562ac035
Luiz Felipe <luizfelipedasilva678@gmail.com>
2023-10-03 20:22:07 -0300

    Adding morgan

--- nasa-project/server/src/app.js
+++ nasa-project/server/src/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const planetsRouter = require("./routes/planets/planets.router");
-const path = require("path");
+const morgan = require("morgan");
 
+const path = require("path");
 const app = express();
 
 app.use(
@@ -10,6 +11,7 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(planetsRouter);
