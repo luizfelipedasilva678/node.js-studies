4201ea08baa237acf83000921b163bcfa035d5fe
Luiz Felipe <luizfelipedasilva678@gmail.com>
2023-10-03 21:50:54 -0300

    Separation of data layer

--- nasa-project/server/src/routes/launches/launches.controller.js
+++ nasa-project/server/src/routes/launches/launches.controller.js
@@ -1,7 +1,7 @@
-const { launches } = require("../../models/launches.model");
+const launchesModel = require("../../models/launches.model");
 
 function getAllLaunches(req, res) {
-  return res.status(200).json(Array.from(launches.values()));
+  return res.status(200).json(launchesModel.getAllLaunches());
 }
 
 module.exports = {
