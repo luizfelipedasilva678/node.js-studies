49cdfcbed1d0f2118344ab87ca41c63bab1bdee1
Luiz Felipe <luizfelipedasilva678@gmail.com>
2023-10-07 16:15:52 -0300

    Mongoose schemas

--- nasa-project/server/src/server.js
+++ nasa-project/server/src/server.js
@@ -1,12 +1,25 @@
+require("dotenv").config();
 const http = require("http");
 const app = require("./app");
+const mongoose = require("mongoose");
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = process.env.MONGO_URL;
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 const server = http.createServer(app);
 
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
