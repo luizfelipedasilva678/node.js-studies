76e2eb7ce784fa95d693a30fcc40d1cdf630152a
Luiz Felipe <luizfelipedasilva678@gmail.com>
2023-11-22 01:01:44 -0300

    Versioning api

--- nasa-project/server/src/app.js
+++ nasa-project/server/src/app.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const planetsRouter = require("./routes/planets/planets.router");
-const launchesRouter = require("./routes/launches/launches.router");
 const morgan = require("morgan");
-
 const path = require("path");
 const app = express();
+const { api } = require("./routes/api");
 
 app.use(
   cors({
@@ -15,8 +13,7 @@ app.use(
 app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
-app.use("/planets", planetsRouter);
-app.use("/launches", launchesRouter);
+app.use("/v1", api);
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
