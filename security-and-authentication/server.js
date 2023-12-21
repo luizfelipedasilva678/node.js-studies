3e13c0f9ea3dbb65b84f7fd7f7c41eba3ec0b0eb
Luiz Felipe <luizfelipedasilva678@gmail.com>
2023-12-20 21:39:20 -0300

    Helmet

--- security-and-authentication/server.js
+++ security-and-authentication/server.js
@@ -2,9 +2,12 @@ const fs = require("fs");
 const https = require("https");
 const path = require("path");
 const express = require("express");
+const helmet = require("helmet");
 
 const PORT = 3000;
 const app = express();
+
+app.use(helmet());
 
 app.get("/secret", (req, res) => {
   return res.send("Your personal secret is 42!");
