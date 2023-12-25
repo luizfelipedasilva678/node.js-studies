5b7075d4ab3b9fdfbb4a1352717992138b86bf69
Luiz Felipe <luizfelipedasilva678@gmail.com>
2023-12-25 00:18:11 -0300

    Starting the game

--- sockets/script.js
+++ sockets/script.js
@@ -2,6 +2,7 @@
 const canvas = document.createElement("canvas");
 const context = canvas.getContext("2d");
 const socket = io("http://localhost:3000");
+let isReferee = false;
 
 let paddleIndex = 0;
 
@@ -161,14 +162,15 @@ function animate() {
   window.requestAnimationFrame(animate);
 }
 
-// Start Game, Reset Everything
-function startGame() {
+// lOAD Game, Reset Everything
+function loadGame() {
   createCanvas();
   renderIntro();
+  socket.emit("ready");
+}
 
-  socket.emit("ready", {});
-
-  paddleIndex = 0;
+function startGame() {
+  paddleIndex = isReferee ? 0 : 1;
   window.requestAnimationFrame(animate);
   canvas.addEventListener("mousemove", (e) => {
     playerMoved = true;
@@ -185,8 +187,15 @@ function startGame() {
 }
 
 // On Load
-startGame();
+loadGame();
 
 socket.on("connect", () => {
   console.log("Connected as...", socket.id);
+});
+
+socket.on("startGame", (refereeId) => {
+  console.log(refereeId);
+
+  isReferee = socket.id === refereeId;
+  startGame();
 });
--- sockets/server.js
+++ sockets/server.js
@@ -11,6 +11,17 @@ const PORT = 3000;
 server.listen(PORT);
 console.log("Listening on port " + PORT);
 
+let readyPlayerCount = 0;
+
 io.on("connection", (socket) => {
   console.log("Connected");
+
+  socket.on("ready", () => {
+    console.log("Player ready", socket.id);
+    readyPlayerCount++;
+
+    if (readyPlayerCount === 2) {
+      io.emit("startGame", socket.id);
+    }
+  });
 });
